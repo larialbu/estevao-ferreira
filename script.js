3e2b564e57ea65d87fcb5ca969fff5b0c701016b
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2023-03-17 19:35:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+   $('.menu-btn').click(function(){
+       $('.navbar .menu').toggleClass("active");
+       $('.menu-btn i').toggleClass("active");
+   });
+   $('.carousel').owlCarousel({
+       margin:20,
+       loop:true,
+       autoplayTimeOut:2000,
+       autoplayHoverPauser:true,
+       responsive:{
+           0:{
+               items:1,
+               nav:false
+           },
+           600:{
+               items:2,
+               nav:false
+           },
+           1000:{
+               items:3,
+               nav:false
+           }
+       }
+   });
+});
